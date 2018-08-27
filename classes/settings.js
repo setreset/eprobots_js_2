57ba6d3fcb3815bb8a363250192f598c611f8cea
Christian Wehr <christian.wehr@gmx.de>
2018-08-27 22:08:54 +0200

    settings..

--- classes/settings.js
+++ classes/settings.js
@@ -11,7 +11,7 @@ class Settings {
         this.sleeptime = 0;
         this.plants_max = 500;
         this.plants_lifetime = 250;
-        this.eprobots_max = 5000;
+        this.eprobots_max = 2500;
         this.eprobots_lifetime = 10000;
         this.eprobots_fossiltime = 400;
         this.PROGRAM_LENGTH = 500;
