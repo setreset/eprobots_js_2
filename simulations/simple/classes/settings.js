a3bdc2e701ab8f736e0d996031bcf3f64d8e56ba
Christian Wehr <christian.wehr@gmx.de>
2018-10-19 08:59:23 +0200

    settings: new world with/height because of new monitor

--- simulations/simple/classes/settings.js
+++ simulations/simple/classes/settings.js
@@ -6,8 +6,8 @@ class Settings {
     constructor() {
         this.background_color = "#000000";
         this.beam_at_borders = false;
-        this.world_width = 720;
-        this.world_height = 450;
+        this.world_width = 960;
+        this.world_height = 540;
         this.sleeptime = 0;
         this.plants_max = 500;
         this.plants_lifetime = 250;
