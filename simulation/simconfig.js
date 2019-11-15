9b5274db14073871ce6c26c33612036f0a8e957b
Christian Wehr <christian.wehr@gmx.de>
2019-11-15 07:13:57 +0100

    eprobotparasit

--- simulation/simconfig.js
+++ simulation/simconfig.js
@@ -19,41 +19,28 @@ var simconfig = [
         "special_energy_no_go_fields": ["eprobot_b"],
         "individuals_max": 500
     },
-    //{
-    //    "eprobot_key": "eproboteater_a",
-    //    "eprobot_class": EprobotEater,
-    //    "base_color": 180,
-    //    "special_energy": false,
-    //    "individuals_max": 300
-    //},
+    {
+        "eprobot_key": "eprobot_p",
+        "eprobot_class": EprobotParasit,
+        "base_color": 180,
+        "special_energy": false,
+        "special_energy_consume_fields": ["eprobot_a", "eprobot_b"],
+        "special_energy_no_go_fields": [],
+        "individuals_max": 200
+    },
+    {
+        "eprobot_key": "eproboteater_a",
+        "eprobot_class": EprobotEater,
+        "base_color": 270,
+        "special_energy": false,
+        "special_energy_no_go_fields": ["eprobot_a", "eprobot_b"],
+        "individuals_max": 200
+    },
     //{
     //    "eprobot_key": "eproboteater_b",
     //    "eprobot_class": EprobotEater,
     //    "base_color": 270,
     //    "special_energy": false,
     //    "individuals_max": 300
-    //}
-
-    //{
-    //    "eprobot_key": "eprobot_a",
-    //    "eprobot_class": EprobotA,
-    //    "base_color": 0,
-    //    "special_energy": true,
-    //    "individuals_max": 500
-    //
-    //},
-    //{
-    //    "eprobot_key": "eprobot_b",
-    //    "eprobot_class": EprobotB,
-    //    "base_color": 120,
-    //    "special_energy": true,
-    //    "individuals_max": 500
-    //},
-    //{
-    //    "eprobot_key": "eprobot_c",
-    //    "eprobot_class": EprobotC,
-    //    "base_color": 240,
-    //    "special_energy": true,
-    //    "individuals_max": 500
     //}
 ];
